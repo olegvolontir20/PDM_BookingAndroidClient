a5f85f91e768440aa35b434f5181313ad99bd2e0
olegvolontir20 <olegvolontir20@gmail.com>
2024-01-31 10:43:28 +0200

    Some ApartmentScreenActivity adjustments

--- app/src/main/java/com/booking/app/ApartmentScreenActivity.kt
+++ app/src/main/java/com/booking/app/ApartmentScreenActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
@@ -25,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,34 +44,57 @@ class ApartmentScreenActivity : ComponentActivity() {
                     ApartmentDetailsScreen(apartment)
                 }
             }
-
-
         }
     }
 }
-
 
 fun getApartment(context: Context, onReceive: (ApartmentModel) -> Unit) {
     val apartment = ApartmentModel()
     onReceive(apartment)
 }
+
+//ApartmentDetailsScreenPreview e doar pentru afisare in preview din dreapta cand apesi "Split" sau "Design",
+//ceasta functie nu ajunge in aplicatie in sine + are scoase variabilele deoarec Preview nu stie sa le arate vizual
+@Preview
 @Composable
-fun ApartmentDetailsScreen(apartment: ApartmentModel) {
-
-    Column(
-        modifier = Modifier.padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-
-        Text(text = " ${apartment.pathImage}")
-        Text(text = "Nume Apartament: ${apartment.name}")
-        Text(text = "Descriere: ${apartment.description}")
-        Text(text = "Preț: ${apartment.price}")
-        // Butonul de rezervare
-        Button(onClick = { /* Logica de rezervare */ }) {
-            Text(text = "Rezervă acum")
+fun ApartmentDetailsScreenPreview()
+{
+    Surface{
+        Column(
+            modifier = Modifier.
+            padding(16.dp).
+            fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            Text(text = " ")
+            Text(text = "Nume Apartament: ")
+            Text(text = "Descriere: ")
+            Text(text = "Preț: ")
+            // Butonul de rezervare
+            Button(onClick = { /* Logica de rezervare */ }) {
+                Text(text = "Rezervă acum")
+            }
         }
     }
+}
 
+@Composable
+fun ApartmentDetailsScreen(apartment: ApartmentModel) {
+    Surface {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+
+            Text(text = " ${apartment.pathImage}")
+            Text(text = "Nume Apartament: ${apartment.name}")
+            Text(text = "Descriere: ${apartment.description}")
+            Text(text = "Preț: ${apartment.price}")
+            // Butonul de rezervare
+            Button(onClick = { /* Logica de rezervare */ }) {
+                Text(text = "Rezervă acum")
+            }
+        }
+    }
 }
 
